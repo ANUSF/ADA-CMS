d854ed0d1e510e6f2b50198061b4ed5511f4a7eb
odf <olaf.delgado@gmail.com>
2012-02-05 17:27:55 +1100

    Removed a logger message.

--- app/controllers/ada_data_controller.rb
+++ app/controllers/ada_data_controller.rb
@@ -5,7 +5,6 @@ class AdaDataController < ApplicationController
     base = File.join('', 'projects_qfs', 'd10', 'assda', 'publish', 'ADAData')
 
     path = File.join base, File.absolute_path(File.join '', params[:rest])
-    Rails.logger.error "@@@ #{path}"
     if path.starts_with?(File.join base, '')
       send_file(path,
                 :filename => File.basename(path),
